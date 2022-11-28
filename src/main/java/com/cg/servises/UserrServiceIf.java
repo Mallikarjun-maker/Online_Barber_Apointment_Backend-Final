f43b3c75f25ae3b0cc533038867adc7275a2dd4a
Mallikarjun-maker <mallikarjun.a.mallikarjun@capgemini.com>
2022-11-28 10:48:58 +0530

    fast commit

---
+++ src/main/java/com/cg/servises/UserrServiceIf.java
@@ -0,0 +1,21 @@
+package com.cg.servises;
+
+import java.util.List;
+
+import com.cg.model.Cart;
+import com.cg.model.Userr;
+
+public interface UserrServiceIf {
+	
+	
+	
+    public Userr SaveUserDetails(Userr user);
+    public Userr fetchUserByemailIdAndPassword(Userr user);
+    public Userr fetchById(int id);
+    public Cart addOrder(int ServiceId , String  useremail);
+
+    
+    
+    
+    
+}
